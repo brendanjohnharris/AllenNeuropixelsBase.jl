0dd6fc596b4f3fe3057a54a345933540de500866
brendanjohnharris <brendanjohnharris@gmail.com>
2026-03-26 22:48:46 +1100

    Tweak

--- src/VisualBehavior.jl
+++ src/VisualBehavior.jl
@@ -31,7 +31,7 @@ Return the URL for the neuropixels visual behavior project manifest file hosted 
 Other manifest version numbers can be identified here: https://s3.console.aws.amazon.com/s3/buckets/visual-behavior-neuropixels-data?prefix=visual-behavior-neuropixels%2Fmanifests%2F&region=us-west-2
 
 """
-function getmanifesturl(manifest_version = "0.5.0")
+function getmanifesturl(manifest_version="0.5.0")
     object_key = "manifests/visual-behavior-neuropixels_project_manifest_v$manifest_version.json"
     return visualbehaviorbehavior * object_key
 end
@@ -54,14 +54,14 @@ Creates a dictionary-like object representing the directory and file structure f
  * dict_tree : A dictionary-like object representing the directory and file structure.
 """
 function filetree(paths::Vector{String})
-    dict_tree = Dict{String, Any}()
+    dict_tree = Dict{String,Any}()
     for path in paths
         current_dict = dict_tree
         components = split(path, '/')
         filter!(!isempty, components)
-        for component in components[1:(end - 1)]
+        for component in components[1:(end-1)]
             if !haskey(current_dict, component)
-                current_dict[component] = Dict{String, Any}()
+                current_dict[component] = Dict{String,Any}()
             end
             current_dict = current_dict[component]
         end
@@ -77,9 +77,9 @@ Get the paths to the visual behavior neuropixels data on Amazon S3 as a tree-lik
 """
 function datapaths()
     data = getmanifest()
-    ks = merge([data[k] for k in keys(data) if data[k] isa Dict]...)
-    ks = getindex.([ks[k] isa Dict ? ks[k] : Dict(k => ks[k]) for k in keys(ks)], ["url"])
-    tree = filetree(String.(ks))
+    all_files = reduce(merge, [data[k] for k in keys(data) if data[k] isa AbstractDict]; init=Dict{String,Any}())
+    urls = [v isa AbstractDict ? v["url"] : all_files[k] for (k, v) in all_files]
+    tree = filetree(String.(urls))
     return tree["https:"]["visual-behavior-neuropixels-data.s3.us-west-2.amazonaws.com"]["visual-behavior-neuropixels"]
 end
 
@@ -120,14 +120,14 @@ function ANB.getlfppath(session::Session, probeid)
     catch # Probe has no file name for some reason. Make a hacky guess
         @warn "Malformed probe object. Guessing path"
         path = joinpath(ANB.behaviormanifest, "visual-behavior-neuropixels-0.5.0",
-                        "behavior_ecephys_sessions",
-                        string(pyconvert(Int,
-                                         session.pyObject.metadata["ecephys_session_id"])),
-                        "probe_$(name)_lfp.nwb")
+            "behavior_ecephys_sessions",
+            string(pyconvert(Int,
+                session.pyObject.metadata["ecephys_session_id"])),
+            "probe_$(name)_lfp.nwb")
     end
 end
 
-function ANB.isinvalidtime(session::Session, probeids = getprobeids(session), times = NaN)
+function ANB.isinvalidtime(session::Session, probeids=getprobeids(session), times=NaN)
     # paths = ANB.getlfppath.((session,), probeids)
     # for path in paths
     #     f = h5open(path)
@@ -143,7 +143,7 @@ function ANB.getlfptimes(session::Session, probeid)
 
     f = h5open(path)
     stem = ANB.resolvenwblfp(session, probeid)
-    times = f["acquisition"][stem][stem * "_data"]["timestamps"][:]
+    times = f["acquisition"][stem][stem*"_data"]["timestamps"][:]
     close(f)
     return times
 end
@@ -194,7 +194,7 @@ getunits() = manifest["project_metadata"]["units.csv"] |> url2df
 function getunitanalysismetricsbysessiontype()
     session_table = ANB.VisualBehavior.getsessiontable()
     units = getunits()
-    sessionmetrics = innerjoin(session_table, metrics, on = :ecephys_session_id)
+    sessionmetrics = innerjoin(session_table, metrics, on=:ecephys_session_id)
 end
 
 function getprobes(session::AbstractNWBSession)
@@ -224,8 +224,8 @@ end
 
 function getprobefile(session::AbstractNWBSession, probeid::Int)
     getprobefile(session,
-                 first(subset(getprobes(session),
-                              :ecephys_probe_id => ByRow(==(probeid))).name))
+        first(subset(getprobes(session),
+            :ecephys_probe_id => ByRow(==(probeid))).name))
 end
 
 function ANB.getepochs(S::Session)
@@ -234,9 +234,9 @@ function ANB.getepochs(S::Session)
     df = groupby(df, :stimulus_block)
     _intersect(x) = minimum(minimum.(x)) .. maximum(maximum.(x))
     df = DataFrames.combine(df, :interval => _intersect => :interval,
-                            :active => unique => :active,
-                            :stimulus_block => unique => :stimulus_block,
-                            :stimulus_name => unique => :stimulus_name)
+        :active => unique => :active,
+        :stimulus_block => unique => :stimulus_block,
+        :stimulus_name => unique => :stimulus_name)
     df.start_time = minimum.(df.interval)
     df.end_time = maximum.(df.interval)
     df.duration = df.end_time - df.start_time
@@ -264,17 +264,17 @@ function ANB.getchannels(S::Session)
     s = py2df(S.pyObject.get_channels())
 end
 
-function getunitanalysismetrics(; filter_by_validity = nothing) # Extra kwarg for Visual Coding compat
+function getunitanalysismetrics(; filter_by_validity=nothing) # Extra kwarg for Visual Coding compat
     session_table = ANB.VisualBehavior.getsessiontable()
     metrics = ANB.behaviorcache().get_unit_table() |> py2df
-    metrics = outerjoin(metrics, session_table; on = :ecephys_session_id)
+    metrics = outerjoin(metrics, session_table; on=:ecephys_session_id)
 end
 
 function ANB.getunitanalysismetrics(session::Session; kwargs...)
     ANB.getunitmetrics(session; kwargs...)
 end
 
-function ANB.getunitmetrics(session::Session; filter_by_validity = false)
+function ANB.getunitmetrics(session::Session; filter_by_validity=false)
     if filter_by_validity
         @warn "Filtering by validity is not implemented for visual behavior neuropixels data"
     end
@@ -298,8 +298,8 @@ end
 
 ## Behavior metrics
 import AllenNeuropixelsBase: gettrials, getlicks, getrewards, geteyetracking,
-                             getrunningspeed, getbehavior, getchangetrials,
-                             getchangetrialtimeseries
+    getrunningspeed, getbehavior, getchangetrials,
+    getchangetrialtimeseries
 function gettrials(S::Session)
     df = S.pyObject.trials |> py2df
     df.interval = [a .. b for (a, b) in zip(df.start_time, df.stop_time)]
@@ -315,7 +315,7 @@ function _getbehavior(S::Session)
     dfs = [f(S) for f in fs]
 end
 
-function stimulustimeseries(session; blanks = true)
+function stimulustimeseries(session; blanks=true)
     df = ANB.getstimuli(session)
     x = TimeseriesBase.Timeseries(df.start_time, df.image_name)
     y = TimeseriesBase.Timeseries(df.end_time, df.image_name)
@@ -335,7 +335,7 @@ function getchangetrials(session)
     # Get corrected change time by matching stimulus frame with start time in stimulus table
     stimuli = ANB.getstimuli(session)
     stimuli.change_frame = stimuli.start_frame
-    stimuli = innerjoin(df, stimuli, on = :change_frame, makeunique = true)
+    stimuli = innerjoin(df, stimuli, on=:change_frame, makeunique=true)
     df.change_time_with_display_delay = stimuli.start_time_1
 
     # Get response (lick) latency
@@ -365,10 +365,10 @@ function getchangetrialtimeseries(session)
 end
 
 flashesset = (Val{:Natural_Images_Lum_Matched_set_ophys_G_2019},
-              Val{:Natural_Images_Lum_Matched_set_ophys_H_2019})
-function ANB.alignlfp(session, X, stimulus::Union{flashesset...}; trail = :change,
-                      trials = nothing, conditions = nothing, nonconditions = nothing,
-                      zero = false)
+    Val{:Natural_Images_Lum_Matched_set_ophys_H_2019})
+function ANB.alignlfp(session, X, stimulus::Union{flashesset...}; trail=:change,
+    trials=nothing, conditions=nothing, nonconditions=nothing,
+    zero=false)
     stimulus = string(typeof(stimulus).parameters[1])
     flash = 0.250 # s see visual behavior white paper
     iti = 0.500 # s
@@ -379,32 +379,32 @@ function ANB.alignlfp(session, X, stimulus::Union{flashesset...}; trail = :chang
         # stimuli = stimuli[idxs, :]
     end
     if !isnothing(trials)
-        is = is[is.trials_id .∈ (trials.trials_id,), :]
+        is = is[is.trials_id.∈(trials.trials_id,), :]
     end
     if !isnothing(conditions)
         for c in conditions
-            is = is[is[:, c] .== true, :]
+            is = is[is[:, c].==true, :]
         end
     end
     if !isnothing(nonconditions)
         for c in nonconditions
-            is = is[is[:, c] .== false, :]
+            is = is[is[:, c].==false, :]
         end
     end
     if trail == :onset # The programmed change time
         onsets = is.start_time
-        is = [onsets[i] .. min(onsets[i + 1], onsets[i] + flash + iti)
-              for i in 1:(length(onsets) - 1)]
+        is = [onsets[i] .. min(onsets[i+1], onsets[i] + flash + iti)
+              for i in 1:(length(onsets)-1)]
     elseif trail == :offset # The programmed off time
         offsets = is.stop_time
         onsets = is.start_time[2:end]
-        is = [offsets[i] .. min(offsets[i + 1], offsets[i] + iti)
-              for i in 1:(length(offsets) - 1)]
+        is = [offsets[i] .. min(offsets[i+1], offsets[i] + iti)
+              for i in 1:(length(offsets)-1)]
     elseif trail == :change # The true change time, including display delay
         onsets = is.change_time_with_display_delay
         onsets = [onsets; Inf]
-        is = [onsets[i] .. min(onsets[i + 1], onsets[i] + flash + iti)
-              for i in 1:(length(onsets) - 1)]
+        is = [onsets[i] .. min(onsets[i+1], onsets[i] + flash + iti)
+              for i in 1:(length(onsets)-1)]
     else
         is = is.interval
     end
@@ -420,4 +420,4 @@ end
 
 end
 export VisualBehavior
-getprobefiles(S::AbstractNWBSession; dataset = VisualBehavior) = dataset.getprobefiles(S)
+getprobefiles(S::AbstractNWBSession; dataset=VisualBehavior) = dataset.getprobefiles(S)
